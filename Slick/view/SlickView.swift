267a4930e00652197841e9810ef074edc59da4d0
Alex Rozanski <alex@rozanski.me>
2022-12-29 12:56:13 -0500

    add extracted colors to SlickView

--- Slick/view/SlickView.swift
+++ Slick/view/SlickView.swift
@@ -7,22 +7,77 @@
 
 import SwiftUI
 
+// Keep this struct internal -- expose a higher-level appearance API if configuratiojn is desired.
+internal struct Appearance {
+  static var `default` = Appearance(
+    blurColors: true,
+    opacity: 0.55,
+    blurRadius: 29.4,
+    horizontalInsets: 22.93,
+    verticalInsets: 5.8
+  )
+
+  let blurColors: Bool
+  let opacity: Double
+  let blurRadius: Double
+  let horizontalInsets: Double
+  let verticalInsets: Double
+}
+
 public struct SlickView<Image>: View where Image: View {
   public typealias ImageViewBuilder = (_ nsImage: NSImage) -> Image
 
+  private let imageColorExtractor = ImageColorExtractor()
+
   private let image: NSImage?
+  private let appearance: Appearance
   private let imageView: ImageViewBuilder
+
+  @State private var backgroundColors: [NSColor]?
 
   public init(_ image: NSImage?, @ViewBuilder imageView: @escaping ImageViewBuilder) {
     self.image = image
+    self.appearance = .default
+    self.imageView = imageView
+  }
+
+  init(_ image: NSImage?, appearance: Appearance, @ViewBuilder imageView: @escaping ImageViewBuilder) {
+    self.image = image
+    self.appearance = appearance
     self.imageView = imageView
   }
 
   @MainActor public var body: some View {
     if let image = image {
       imageView(image)
-        .padding()
-        .background(.red)
+        .onAppear {
+          recalculateColors(from: image)
+        }
+        .onChange(of: image) { newImage in
+          recalculateColors(from: newImage)
+        }
+        .padding(.horizontal, appearance.horizontalInsets)
+        .padding(.vertical, appearance.verticalInsets)
+        .background(backgroundGradient)
     }
+  }
+
+  @ViewBuilder private var backgroundGradient: some View {
+    if let backgroundColors = backgroundColors {
+      Rectangle()
+        .fill(AngularGradient(gradient: Gradient(
+          colors: backgroundColors.map { Color(cgColor: $0.cgColor)}
+        ), center: .center, angle: .degrees(225)))
+        .opacity(appearance.blurColors ? appearance.opacity : 1)
+        .blur(radius: appearance.blurColors ? appearance.blurRadius : 0)
+        .blendMode(appearance.blurColors ? .multiply : .normal)
+    }
+  }
+
+  private func recalculateColors(from image: NSImage) {
+    let colors = imageColorExtractor.extractColors(from: image)
+    var wrappedColors = colors
+    colors.first.map { wrappedColors.append($0) }
+    backgroundColors = wrappedColors
   }
 }
--- SlickExample/ContentView.swift
+++ SlickExample/ContentView.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 import Slick
 
+let images = ["astronaut", "frog"]
+
 struct ContentView: View {
+  @State var selectedImage = "astronaut"
+
   var body: some View {
-    HStack {
-      SlickView(NSImage(named: "astronaut")) { nsImage in
-        Image(nsImage: nsImage)
-          .resizable()
-          .aspectRatio(contentMode: .fit)
+    VStack {
+      Picker("Image", selection: $selectedImage) {
+        ForEach(images, id: \.self) {
+          Text($0)
+        }
+      }
+      HStack {
+        SlickView(NSImage(named: selectedImage)) { nsImage in
+          Image(nsImage: nsImage)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+        }
       }
     }
     .padding()
