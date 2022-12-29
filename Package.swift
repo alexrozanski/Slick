0f37f72f6d7ea1040e23dcb11f09874c660fb288
Alex Rozanski <alex@rozanski.me>
2022-12-29 15:21:38 -0500

    add path key for package

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
   targets: [
     .target(
       name: "Slick",
-      dependencies: []),
+      dependencies: [],
+      path: "Slick"),
   ],
   swiftLanguageVersions: [.v5]
 )
