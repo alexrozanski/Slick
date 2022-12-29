5f58bbfd3ffe3451f0626def4380fec917ab8737
Alex Rozanski <alex@rozanski.me>
2022-12-29 14:39:44 -0500

    capitalise package name

--- Package.swift
+++ Package.swift
@@ -1,22 +1,21 @@
 // swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-  name: "slick",
+  name: "Slick",
   platforms: [
     .macOS(.v10_15)
   ],
   products: [
     .library(
-      name: "slick",
-      targets: ["slick"]),
+      name: "Slick",
+      targets: ["Slick"]),
   ],
   dependencies: [],
   targets: [
     .target(
-      name: "slick",
+      name: "Slick",
       dependencies: []),
-  ]
+  ],
+  swiftLanguageVersions: [.v5]
 )
