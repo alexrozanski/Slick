0f01ddb5d2b48e2b4dadddc24a77978c48ef6db0
Alex Rozanski <alex@rozanski.me>
2023-01-02 17:37:56 -0500

    fix debug view scroll view width

--- Slick/debug/SlickDebugView.swift
+++ Slick/debug/SlickDebugView.swift
@@ -9,27 +9,26 @@ import SwiftUI
 
 public struct SlickDebugView: View {
   @Environment(\.debugInfo) var debugInfo: DebugInfo?
-
+  
   public init() {}
-
+  
   public var body: some View {
     if let debugInfo = debugInfo {
-      VStack {
-        ScrollView {
-          VStack {
-            ForEach(Array(debugInfo.info.keys.sorted(by: { p1, p2 in p1.angle < p2.angle })), id: \.self) { position in
-              if let info = debugInfo.info[position] {
-                DebugCornerView(
-                  label: position.label,
-                  sampledImage: info.image,
-                  colors: info.colors
-                )
-                .padding(.bottom, 24)
-              }
+      ScrollView {
+        VStack {
+          ForEach(Array(debugInfo.info.keys.sorted(by: { p1, p2 in p1.angle < p2.angle })), id: \.self) { position in
+            if let info = debugInfo.info[position] {
+              DebugCornerView(
+                label: position.label,
+                sampledImage: info.image,
+                colors: info.colors
+              )
+              .padding(.bottom, 24)
             }
           }
-          .padding()
         }
+        .padding()
+        .frame(maxWidth: .infinity)
       }
     }
   }
