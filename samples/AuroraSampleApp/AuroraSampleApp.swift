86e428e3dab36c8819725fac6bd803d3a044e94c
Alex Rozanski <alex@rozanski.me>
2023-01-06 17:12:20 -0500

    add AuroraSampleApp as main example app

---
+++ samples/AuroraSampleApp/AuroraSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuroraSampleApp.swift
+//  AuroraSampleApp
+//
+//  Created by Alex Rozanski on 06/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AuroraSampleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
